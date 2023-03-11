3a233ef23d480301ef8f949a8240d4806f293f8f
Bartosz Chorazy <bartosz.chorazy.serwisy@gmail.com>
2023-03-11 14:04:53 +0100

    eslint github test passed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,22 +14,9 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
   it(`should have as title 'english-dictionary'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('english-dictionary');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('english-dictionary app is running!');
+    expect(app.title).toEqual('english-dictionary2');
   });
 });
